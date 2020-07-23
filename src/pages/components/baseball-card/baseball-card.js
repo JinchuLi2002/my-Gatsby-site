74c3acc3ad7f5e1b103d57075854311dc3ef9052
JinchuLi2002 <63861808+JinchuLi2002@users.noreply.github.com>
2020-07-23 06:18:35 -0700

    baseball card update 1

--- src/pages/components/baseball-card/baseball-card.js
+++ src/pages/components/baseball-card/baseball-card.js
@@ -5,15 +5,10 @@ import React from "react"
 
 const Card = (props) => {
 	const style = {
-		backgroundImage: "url("+ props.player.frontImageUrl +")",
-		backgroundPosition: "center center",
-		backgroundSize: "auto 100%"
+		
 	}
 	return(
-		<div 
- 			className="card front" 
- 			style={ style }
- 		>
+		<div>
  			
  		</div>
 	);
